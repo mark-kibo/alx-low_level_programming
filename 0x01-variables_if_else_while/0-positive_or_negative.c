07f1129a481ac34da7f1a6ed0f231dd1e5e85334
mark-kibo <kibochamark@gmail.com>
2022-07-06 00:43:06 -0700

    new

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+/* betty style doc for function main goes there */
+
+int main(void)
+
+{
+
+					int n;
+
+
+
+					srand(time(0));
+
+					n = rand() - RAND_MAX / 2;
+
+					/* your code goes there */
+					printf("%d", n);
+
+					return (0);
+
+}
