9000753b812a06b76d8ae25ee2e5d59c30864ce9
mark-kibo <kibochamark@gmail.com>
2022-07-06 02:48:06 -0700

    new

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,30 +9,18 @@ int main(void)
 
 	char l;
 
-
 	l = 'a';
-
 	while (l <= 'z')
 	{
-
 		putchar(l);
-
 		l++;
-
 	}
-
 	l = 'A';
-	
 	while (l <= 'Z')
 	{
-
 		putchar(l);
-
 		l++;
-
 	}
-
 	putchar('\n');
-
 	return (0);
 }
