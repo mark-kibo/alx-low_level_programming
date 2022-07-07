ba5b4fba283646e314384acb58c5ec8aa7c1ab5f
mark-kibo <kibochamark@gmail.com>
2022-07-07 02:05:58 -0700

    new

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+
+
+/**
+
+ * add - adds two integers
+
+ * @a: first integer
+
+ * @b: second integer
+
+ *
+
+ * Return: addition of a and b
+
+ */
+
+
+
+int add(int a, int b)
+
+{
+
+	return (a + b);
+
+}
