8ad44f12b3348338f8b0be617f8a6abf2d5b8e28
mark-kibo <kibochamark@gmail.com>
2022-07-07 06:44:53 -0700

    new

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,14 +11,19 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
+
 		for (c = 0; c <= 14; c++)
 		{
+
 			if (c < 10)
+
 				b = c;
+
 			else
+
 			{
-				a = c / 10;
-			       	b = c % 10;
+
+				a = c / 10; b = c % 10;
 				_putchar('0' + a);
 
 			}
