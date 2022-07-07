90c086fbb69c2bd2cb1543e8d147e928e27be3af
mark-kibo <kibochamark@gmail.com>
2022-07-07 01:41:06 -0700

    new

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,30 @@
 #include "main.h"
 /**
  * main - entry point
- * 
- * Description: Print _putchar
+ *
+ * Description: Prints Holberton with _putchar
  *
  * Return: always 0
  */
 int main(void)
 {
+
 	int i = 0;
 
 	char c;
 
-	char s[] = "_putchar\n";
-	while (i <= 8)
+	char s[] = "Holberton\n";
+
+
+
+	while (i <= 9)
 	{
+
 		c = s[i];
 		_putchar(c);
+
 		i++;
+
 	}
 	return (0);
 }
