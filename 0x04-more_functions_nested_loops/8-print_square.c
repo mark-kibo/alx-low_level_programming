15bf73b6d79c914600a4b28857220023f4151542
mark-kibo <kibochamark@gmail.com>
2022-07-07 23:57:58 -0700

    new

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,9 +15,9 @@ void print_square(int size)
 
 	for (a = 0; a < size; a++)
 	{
-		for (b = 0; b < (size); b++)
+		for (b = 0; b < size; b++)
 		{
-			_putchar('#')
+			_putchar('#');
 		}
 		_putchar('\n');
 	}
