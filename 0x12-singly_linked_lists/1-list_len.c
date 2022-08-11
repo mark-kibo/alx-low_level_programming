4a1d7866603fa16f8f6a22637af714911c2f779f
Anet <ndwigaannetr13@gmail.com>
2022-08-11 13:39:55 -0700

    feat:modify file

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
-
+/**
+ * list_len - finds the number of elements in a linked list
+ * @h: pointer to the start of the linked list
+ *
+ * Return: number of elements in the linked list
+ */
 size_t list_len(const list_t *h)
 {
 	const list_t *tmp;
+	unsigned int i;
 
-	unassigned int i;
-	
 	tmp = h;
-	for(i=0; tmp; i++)
-	{
+	for (i = 0; tmp; i++)
 		tmp = tmp->next;
-	}
 	return (i);
 }
