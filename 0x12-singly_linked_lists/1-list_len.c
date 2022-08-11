31681ec7eda12a9d3773c8b7064e7ef3289d6696
Anet <ndwigaannetr13@gmail.com>
2022-08-11 13:29:22 -0700

    build:create file lisst_len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+size_t list_len(const list_t *h)
+{
+	const list_t *head;
+
+	int count;
+
+	unassigned int;
+	
+	head=h;
+	for(i=0; head; i++)
+	{
+		count = count + 1;
+	}
+	return count;
+}
