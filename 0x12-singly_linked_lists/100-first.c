fe553023f7a067883a2bc7c1125227384dafefca
mark-kibo <kibochamark@gmail.com>
2022-08-11 14:10:52 -0700

    build:advanced task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * create a function that prints the following words
+ *
+ */
+void print(){
+
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	return 0;
+}
+int main(){
+
+	print();
+	return 0;
+}
+
