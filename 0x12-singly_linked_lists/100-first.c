16303957792bf7a8a4dfd44a2a0034bb157d0ca1
mark-kibo <kibochamark@gmail.com>
2022-08-11 14:14:37 -0700

    build:advanced task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,12 +5,7 @@
  */
 void print(){
 
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 	return 0;
 }
-int main(){
-
-	print();
-	return 0;
-}
-
