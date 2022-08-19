f5bdeb81bbacb232b6aab23984dc3682de74a8a5
mark-kibo <kibochamark@gmail.com>
2022-08-19 13:49:56 -0700

    feat:modify task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 /*
  * File: 0-binary_to_uint.c
  */
-#include "holberton.h"
+#include "main.h"
 /**
  * binary_to_uint - Converts a binary number to an unsigned int.
  * @b: A pointer to a string of 0 and 1 chars.
